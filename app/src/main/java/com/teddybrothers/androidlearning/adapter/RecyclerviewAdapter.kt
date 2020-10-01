34dcb13477f34606c1c8e089e386b98358320194
Federico <federico@selfdrvn.com>
2020-10-01 13:50:53 +0800

    MainActivity.kt - implement custom on scroll recyclerview with PaginationScrollListener.kt list_item_loading.xml - create for loading state while calling next page RecyclerviewAdapter.kt - create loading view holder and determine and notify type is a movie item or loading

--- app/src/main/java/com/teddybrothers/androidlearning/adapter/RecyclerviewAdapter.kt
+++ app/src/main/java/com/teddybrothers/androidlearning/adapter/RecyclerviewAdapter.kt
@@ -5,27 +5,72 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.teddybrothers.androidlearning.R
+import com.teddybrothers.androidlearning.databinding.ListItemLoadingBinding
 import com.teddybrothers.androidlearning.databinding.ListItemMovieBinding
 import com.teddybrothers.androidlearning.model.Movie
 
-class RecyclerviewAdapter(private val listener: RecyclerViewListener) : RecyclerView.Adapter<RecyclerviewAdapter.MainViewHolder>() {
-    private var movieList = ArrayList<Movie>()
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
-        return MainViewHolder(
-            DataBindingUtil.inflate(
-                LayoutInflater.from(parent.context),
-                R.layout.list_item_movie,
-                parent,
-                false
-            ),
-            listener
-        )
+class RecyclerviewAdapter(private val listener: RecyclerViewListener) :
+    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    private var movieList = ArrayList<Movie>()
+    private val VIEW_TYPE_LOADING = 0
+    private val VIEW_TYPE_NORMAL = 1
+    private var isLoaderVisible = false
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        if (viewType == VIEW_TYPE_NORMAL) {
+            return MainViewHolder(
+                DataBindingUtil.inflate(
+                    LayoutInflater.from(parent.context),
+                    R.layout.list_item_movie,
+                    parent,
+                    false
+                ),
+                listener
+            )
+        } else {
+            return ProgressViewHolder(
+                DataBindingUtil.inflate(
+                    LayoutInflater.from(parent.context),
+                    R.layout.list_item_loading,
+                    parent,
+                    false
+                )
+            )
+        }
     }
 
-    override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
-        val gigData = this.movieList[position]
-        holder.updateData(gigData)
+    override fun getItemViewType(position: Int): Int {
+        return if (isLoaderVisible) {
+            if (position == movieList.size - 1) VIEW_TYPE_LOADING else VIEW_TYPE_NORMAL
+        } else {
+            VIEW_TYPE_NORMAL
+        }
+    }
+
+    fun addLoading() {
+        isLoaderVisible = true
+        notifyItemInserted(movieList.size)
+    }
+
+    fun removeLoading() {
+        isLoaderVisible = false
+        val position: Int = movieList.size - 1
+        movieList.removeAt(position)
+        notifyItemRemoved(position)
+
+    }
+
+    fun clear() {
+        movieList.clear()
+        notifyDataSetChanged()
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val movie = this.movieList[position]
+        if (holder is MainViewHolder) {
+            holder.updateData(movie)
+        }
     }
 
     override fun getItemCount() = movieList.size
@@ -38,8 +83,8 @@ class RecyclerviewAdapter(private val listener: RecyclerViewListener) : Recycler
     }
 
 
-
-    class MainViewHolder(val binding: ListItemMovieBinding, listener: RecyclerViewListener) : RecyclerView.ViewHolder(binding.root) {
+    class MainViewHolder(val binding: ListItemMovieBinding, listener: RecyclerViewListener) :
+        RecyclerView.ViewHolder(binding.root) {
         private lateinit var movie: Movie
 
         init {
@@ -48,9 +93,13 @@ class RecyclerviewAdapter(private val listener: RecyclerViewListener) : Recycler
             }
         }
 
+
         fun updateData(movie: Movie) {
             this.movie = movie
             binding.movie = movie
         }
     }
+
+    class ProgressViewHolder(binding: ListItemLoadingBinding) :
+        RecyclerView.ViewHolder(binding.root)
 }
--- app/src/main/java/com/teddybrothers/androidlearning/view/MainActivity.kt
+++ app/src/main/java/com/teddybrothers/androidlearning/view/MainActivity.kt
@@ -10,9 +10,12 @@ import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout.OnRefreshListener
 import com.google.gson.Gson
 import com.teddybrothers.androidlearning.R
+import com.teddybrothers.androidlearning.adapter.PaginationScrollListener
+import com.teddybrothers.androidlearning.adapter.PaginationScrollListener.Companion.PAGE_START
 import com.teddybrothers.androidlearning.adapter.RecyclerViewListener
 import com.teddybrothers.androidlearning.adapter.RecyclerviewAdapter
 import com.teddybrothers.androidlearning.model.Movie
+import com.teddybrothers.androidlearning.model.MovieListOutput
 import com.teddybrothers.androidlearning.viewmodel.MovieViewModel
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -27,6 +30,11 @@ class MainActivity : AppCompatActivity(),
     private lateinit var recyclerviewAdapter: RecyclerviewAdapter
     private var listOfMovies = ArrayList<Movie>()
     lateinit var movieViewModel: MovieViewModel
+    private var currentPage: Int = PAGE_START
+    private var isLastPage = false
+    private var isLoading = false
+
+    var itemCount = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,48 +50,90 @@ class MainActivity : AppCompatActivity(),
     private fun init() {
 
         swipeRefreshLayout.setOnRefreshListener(this)
+
         movieViewModel = ViewModelProviders.of(this).get<MovieViewModel>(
             MovieViewModel::class.java
         )
-        recyclerview.layoutManager = LinearLayoutManager(this)
+
+        val linearLayoutManager = LinearLayoutManager(this)
+        recyclerview.layoutManager = linearLayoutManager
         recyclerviewAdapter =
             RecyclerviewAdapter(this)
         recyclerview.adapter = recyclerviewAdapter
 
+        recyclerview.addOnScrollListener(object : PaginationScrollListener(linearLayoutManager) {
+            override fun loadMoreItems() {
+                isLoading = true
+                currentPage++
+                getMovieList()
+            }
+
+            override fun isLastPage(): Boolean {
+                return isLastPage
+            }
+
+            override fun isLoading(): Boolean {
+                return isLoading
+            }
+        });
+
     }
 
     private fun getMovieList() {
-        movieViewModel.getMoviesRepository(RELEASE_DATE_DESC, 1)
-            .observe(this, Observer { movieListResponse ->
+        val call = movieViewModel.getMoviesRepository(RELEASE_DATE_DESC, currentPage)
+        val observer = Observer<MovieListOutput> { movieListResponse ->
+            if (movieListResponse != null) {
                 val movieListResults: List<Movie>? = movieListResponse.results
-                println("list = $movieListResults")
-                listOfMovies.clear()
                 listOfMovies.addAll(movieListResults!!.toList())
-                recyclerviewAdapter.updateDataSet(listOfMovies)
                 swipeRefreshLayout.disableRefresh()
-            })
+
+                if (currentPage != PAGE_START)
+                    recyclerviewAdapter.removeLoading()
+
+                recyclerviewAdapter.updateDataSet(listOfMovies)
+                // check weather is last page or not
+                if (movieListResults.isNotEmpty()) {
+                    recyclerviewAdapter.addLoading()
+                } else {
+                    isLastPage = true
+                }
+                isLoading = false
+            }
+
+        }
+        if (!call.hasActiveObservers()) {
+            call.observe(this, observer)
+        }
     }
 
-    private fun SwipeRefreshLayout.disableRefresh(){
-        postDelayed({ isRefreshing = false },1500)
+    private fun SwipeRefreshLayout.disableRefresh() {
+        postDelayed({ isRefreshing = false }, 1500)
     }
 
-    private fun enableRefresh(){
+    private fun enableRefresh() {
         swipeRefreshLayout.post {
             swipeRefreshLayout.isRefreshing = true
-            getMovieList()}
+            getMovieList()
+        }
     }
 
     override fun onClickListener(item: Any, position: Int) {
         if (item is Movie) {
-            startActivity(Intent(this,
-                DetailMovieActivity::class.java).apply {
-                putExtra("movie",Gson().toJson(item))
+            startActivity(Intent(
+                this,
+                DetailMovieActivity::class.java
+            ).apply {
+                putExtra("movie", Gson().toJson(item))
             })
         }
     }
 
     override fun onRefresh() {
+        itemCount = 0
+        currentPage = PAGE_START
+        isLastPage = false
+        recyclerviewAdapter.clear()
+        listOfMovies.clear()
         enableRefresh()
     }
 
